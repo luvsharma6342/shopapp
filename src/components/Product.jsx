ebbbbe9dfcf4fa5da2c232b40acd6541c788d64c
Luv Sharma <luvsharma105@gmail.com>
2024-04-12 11:47:57 +0530

    fourth commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -38,7 +38,7 @@ const Product = ({ post }) => {
                 </div>
 
                 {
-                    cart.some((p) => p.id == post.id) ?
+                    cart.some((p) => p.id === post.id) ?
                         (<button
                             className='text-gray-700 border-2 border-gray-700 rounded-full font-semibold text-[12px] p-1 px-3 uppercase hover:bg-gray-700 hover:text-white transition duration-300 ease-in'
                             onClick={removeFromCart}>
